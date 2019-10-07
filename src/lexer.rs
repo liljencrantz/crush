9511d3856e9424229931349fc30cf15598c5ea39
Axel Liljencrantz <liljencranz@gmail.com>
2019-10-07 18:53:01 +0200

    Unit tests and bug fixes for lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -38,7 +38,7 @@ pub struct Lexer {
 }
 
 lazy_static! {
-    static ref lex_data: [(TokenType, Regex); 16] = [
+    static ref LEX_DATA: [(TokenType, Regex); 18] = [
         (TokenType::Separator, Regex::new("^;").unwrap()),
         (TokenType::Pipe, Regex::new(r"^\|").unwrap()),
 
@@ -48,11 +48,14 @@ lazy_static! {
         (TokenType::LessThanOrEqual, Regex::new(r"^<=").unwrap()),
         (TokenType::GreaterThan, Regex::new(r"^>").unwrap()),
         (TokenType::GreaterThanOrEqual, Regex::new(r"^>=").unwrap()),
+        (TokenType::NotEqual, Regex::new(r"^!=").unwrap()),
+
+        (TokenType::Integer, Regex::new(r"^[0-9]+").unwrap()),
 
         (TokenType::BlockStart, Regex::new(r"^[`r$*%]?\{").unwrap()),
         (TokenType::BlockEnd, Regex::new(r"^\}").unwrap()),
-        (TokenType::String, Regex::new(r"^[a-zA-Z][-+_a-z-A-Z0-9]*").unwrap()),
-        (TokenType::Wildcard, Regex::new(r"^[a-zA-Z*.?][-+_a-z-A-Z0-9*.?]*").unwrap()),
+        (TokenType::String, Regex::new(r"^[-.+_a-zA-Z][-.+_a-z-A-Z0-9]*").unwrap()),
+        (TokenType::Wildcard, Regex::new(r"^[-.+_a-zA-Z*.?][-+_a-z-A-Z0-9*.?]*").unwrap()),
         (TokenType::Comment, Regex::new("(?m)^#.*$").unwrap()),
         (TokenType::Whitespace, Regex::new(r"^\s+").unwrap()),
         (TokenType::QuotedString, Regex::new(r#"^"([^\\"]|\\.)*""#).unwrap()),
@@ -75,7 +78,7 @@ impl Lexer {
         if self.idx >= self.input.len() {
             return (EOF, 0, 0);
         }
-        for (token, re) in lex_data.into_iter() {
+        for (token, re) in LEX_DATA.into_iter() {
             //let re = Regex::new(r".");
             match re.find(&self.input[self.idx..]) {
                 Some(mat) => {
@@ -126,22 +129,90 @@ impl Lexer {
     }
 }
 
-pub fn do_lex_test() {
-    let mut l = Lexer::new(&String::from("a %{b} == b"));
-    loop {
-        match l.peek().0 {
-            TokenType::Error => {
-                println!("Error");
+#[cfg(test)]
+mod tests {
+    use crate::lexer::Lexer;
+    use crate::lexer::TokenType;
+
+    fn tokens(lexer: &mut Lexer) -> Vec<TokenType> {
+        let mut res: Vec<TokenType> = Vec::new();
+        loop {
+            let t = lexer.pop().0;
+            res.push(t);
+            if (t == TokenType::EOF || t == TokenType::Error) {
                 break;
-            }
-            TokenType::EOF => {
-                println!("Eof");
-                break;
-            }
-            _ => {
-                println!("{:?} {}", l.peek().0, l.peek().1);
             }
         }
-        l.pop();
+        return res;
+    }
+
+    #[test]
+    fn blocks() {
+        let mut l = Lexer::new(&String::from("echo %{foo}"));
+        let tt = tokens(&mut l);
+        assert_eq!(tt, vec![
+            TokenType::String, TokenType::BlockStart, TokenType::String, TokenType::BlockEnd,
+            TokenType::EOF]);
+    }
+
+    #[test]
+    fn globs() {
+        let mut l = Lexer::new(&String::from("echo foo.* abc??def"));
+        let tt = tokens(&mut l);
+        assert_eq!(tt, vec![
+            TokenType::String, TokenType::Wildcard, TokenType::Wildcard, TokenType::EOF]);
+    }
+
+    #[test]
+    fn separators() {
+        let mut l = Lexer::new(&String::from("a|b;c"));
+        let tt = tokens(&mut l);
+        assert_eq!(tt, vec![
+            TokenType::String, TokenType::Pipe, TokenType::String, TokenType::Separator, TokenType::String, TokenType::EOF]);
+    }
+
+    #[test]
+    fn comments() {
+        let mut l = Lexer::new(&String::from("a # this is a comment"));
+        let tt = tokens(&mut l);
+        assert_eq!(tt, vec![
+            TokenType::String, TokenType::EOF]);
+    }
+
+    #[test]
+    fn numbers() {
+        let mut l = Lexer::new(&String::from("b 2 d"));
+        let tt = tokens(&mut l);
+        assert_eq!(tt, vec![
+            TokenType::String, TokenType::Integer, TokenType::String, TokenType::EOF]);
+    }
+
+    #[test]
+    fn quoted_string() {
+        let mut l = Lexer::new(&String::from(r##" "abc"  "\" ggg" "\d \\"  "##));
+        let tt = tokens(&mut l);
+        assert_eq!(tt, vec![
+            TokenType::QuotedString, TokenType::QuotedString,
+            TokenType::QuotedString, TokenType::EOF]);
+    }
+
+    #[test]
+    fn assign() {
+        let mut l = Lexer::new(&String::from("foo=bar baz = 7"));
+        let tt = tokens(&mut l);
+        assert_eq!(tt, vec![
+            TokenType::String, TokenType::Assign, TokenType::String,
+            TokenType::String, TokenType::Assign, TokenType::Integer,
+            TokenType::EOF,
+        ]);
+    }
+
+    #[test]
+    fn comparison_operators() {
+        let mut l = Lexer::new(&String::from("== >= > < <= !="));
+        let tt = tokens(&mut l);
+        assert_eq!(tt, vec![
+            TokenType::Equal, TokenType::GreaterThanOrEqual, TokenType::GreaterThan,
+            TokenType::LessThan, TokenType::LessThanOrEqual, TokenType::NotEqual, TokenType::EOF]);
     }
 }
