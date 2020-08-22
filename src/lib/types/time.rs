f1d538d6a70c5b2f91af4289f973bd4847a2b635
Axel Liljencrantz <liljencranz@gmail.com>
2020-08-22 14:05:00 +0200

    Add time:format method

--- src/lib/types/time.rs
+++ src/lib/types/time.rs
@@ -11,7 +11,7 @@ use lazy_static::lazy_static;
 use ordered_map::OrderedMap;
 use signature::signature;
 use std::cmp::max;
-use time::strptime;
+use time::{strptime, strftime};
 
 fn full(name: &'static str) -> Vec<&'static str> {
     vec!["global", "types", "time", name]
@@ -50,6 +50,7 @@ lazy_static! {
         );
         // TODO: why unused?
         let _ = Parse::declare_method(&mut res, &path);
+        let _ = Format::declare_method(&mut res, &path);
         res
     };
 }
@@ -74,7 +75,33 @@ fn now(context: CommandContext) -> CrushResult<()> {
 parse,
 can_block=false,
 output=Known(ValueType::Time),
-short="Parse a time string using a strptime-style pattern string")]
+short="Parse a time string using a strptime-style pattern string",
+long="The following format codes are recognised in the format string:",
+long=" * %a, weekday as abbreviated name.",
+long=" * %A, weekday as full name.",
+long=" * %w, weekday as a number, where 0 is Sunday and 6 is Saturday.",
+long=" * %d, day of month, zero padded.",
+long=" * %b, abbreviated month name.",
+long=" * %B, month name.",
+long=" * %m, month, zero padded.",
+long=" * %y, year without century, zero padded.",
+long=" * %Y, year with century.",
+long=" * %H, hour (24-hour clock) as a zero-padded number.",
+long=" * %I, hour (12-hour clock) as a zero-padded number.",
+long=" * %p, locale’s equivalent of either AM or PM.",
+long=" * %M, minute as a zero-padded number.",
+long=" * %S, second as a zero-padded number.",
+long=" * %f, microsecond as a decimal number, zero-padded.",
+long=" * %z, UTC offset in the form +HHMM or -HHMM.",
+long=" * %Z, time zone name.",
+long=" * %j, day of the year, zero-padded.",
+long=" * %U, week number of the year (Sunday as first day of week).",
+long=" * %W, week number of the year (Monday as first day of week).",
+long=" * %c, default data and time representation of current locale.",
+long=" * %x, default date representation of current locale.",
+long=" * %X, default time representation of current locale.",
+long=" * %%, a literal % character."
+)]
 struct Parse {
     #[description("the format of the time.")]
     format: String,
@@ -101,4 +128,46 @@ fn parse(context: CommandContext) -> CrushResult<()> {
         .with_nanosecond(tm.tm_nsec as u32)
         .unwrap();
     context.output.send(Value::Time(dt))
+}
+
+#[signature(
+format,
+can_block=false,
+output=Known(ValueType::String),
+short="Format this time using a strftime-style pattern string",
+long="The following format codes are recognised in the format string:",
+long=" * %a, weekday as abbreviated name.",
+long=" * %A, weekday as full name.",
+long=" * %w, weekday as a number, where 0 is Sunday and 6 is Saturday.",
+long=" * %d, day of month, zero padded.",
+long=" * %b, abbreviated month name.",
+long=" * %B, month name.",
+long=" * %m, month, zero padded.",
+long=" * %y, year without century, zero padded.",
+long=" * %Y, year with century.",
+long=" * %H, hour (24-hour clock) as a zero-padded number.",
+long=" * %I, hour (12-hour clock) as a zero-padded number.",
+long=" * %p, locale’s equivalent of either AM or PM.",
+long=" * %M, minute as a zero-padded number.",
+long=" * %S, second as a zero-padded number.",
+long=" * %f, microsecond as a decimal number, zero-padded.",
+long=" * %z, UTC offset in the form +HHMM or -HHMM.",
+long=" * %Z, time zone name.",
+long=" * %j, day of the year, zero-padded.",
+long=" * %U, week number of the year (Sunday as first day of week).",
+long=" * %W, week number of the year (Monday as first day of week).",
+long=" * %c, default data and time representation of current locale.",
+long=" * %x, default date representation of current locale.",
+long=" * %X, default time representation of current locale.",
+long=" * %%, a literal % character."
+)]
+struct Format {
+    #[description("the format of the time.")]
+    format: String,
+}
+
+fn format(context: CommandContext) -> CrushResult<()> {
+    let time = context.this.time()?;
+    let cfg: Format = Format::parse(context.arguments, &context.printer)?;
+    context.output.send(Value::String(time.format(&cfg.format).to_string()))
 }
