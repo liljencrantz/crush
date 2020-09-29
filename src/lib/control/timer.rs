757bd6927deca857299b0f354fdbe5ff20e72df7
Axel Liljencrantz <liljencranz@gmail.com>
2020-09-29 22:24:46 +0200

    Flesh out API of timer command

--- src/lib/control/timer.rs
+++ src/lib/control/timer.rs
@@ -1,40 +1,43 @@
 use crate::lang::errors::{CrushResult, to_crush_error};
 use crate::lang::execution_context::CommandContext;
 use signature::signature;
-use chrono::Duration;
+use chrono::{Duration, Local, DateTime};
 use crate::lang::value::{Value, ValueType};
 use crate::lang::data::table::{ColumnType, Row};
 
-#[signature(
-r#timer,
-short = "Passes a stream of empty values to act as a reoccurring timer",
-)]
+#[signature(timer, short="Passes a stream of empty rows to act as a reoccurring timer", )]
 pub struct Timer {
     #[description("the interval between heartbeats")]
     interval: Duration,
-    #[description("the number of heartbeats to send. If unspecified, timer will never stop.")]
-    count: Option<i128>,
+    #[description("the delay for the first heartbeat")]
+    initial_delay: Option<Duration>,
+    #[description("if heart beat delivery starts blocking, catch up by sending more heartbeats afterwards.")]
+    #[default(false)]
+    schedule_at_fixed_rate: bool,
 }
 
 fn timer(context: CommandContext) -> CrushResult<()> {
     let cfg: Timer = Timer::parse(context.arguments.clone(), &context.global_state.printer())?;
+    let output = context.output.initialize(vec![])?;
 
-    let output = context.output.initialize(vec![
-        ColumnType::new("beat", ValueType::Empty)
-    ])?;
+    if let Some(initial_delay) = &cfg.initial_delay {
+        std::thread::sleep(to_crush_error(initial_delay.to_std())?);
+    }
 
-    match cfg.count {
-        None => {
-            loop {
-                output.send(Row::new(vec![Value::Empty()]))?;
-                std::thread::sleep(to_crush_error(cfg.interval.to_std())?);
+    if cfg.schedule_at_fixed_rate {
+        let mut last_time = Local::now();
+        loop {
+            output.send(Row::new(vec![]))?;
+            last_time = last_time + cfg.interval.clone();
+            let next_duration = last_time - Local::now();
+            if next_duration > Duration::seconds(0) {
+                std::thread::sleep(to_crush_error(next_duration.to_std())?);
             }
         }
-        Some(count) => {
-            for _ in 0..count {
-                output.send(Row::new(vec![Value::Empty()]))?;
-                std::thread::sleep(to_crush_error(cfg.interval.to_std())?);
-            }
+    } else {
+        loop {
+            output.send(Row::new(vec![]))?;
+            std::thread::sleep(to_crush_error(cfg.interval.to_std())?);
         }
     }
     Ok(())
